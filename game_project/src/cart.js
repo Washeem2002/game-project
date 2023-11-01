8965fdc3013ad831c5eb53955c3c10119632c7e7
Washeem2002 <jkrowling7702@gmail.com>
2023-11-01 11:16:11 +0530

    getting to finish

--- game_project/src/cart.js
+++ game_project/src/cart.js
@@ -63,7 +63,7 @@ const Cart=()=>{
     
      const remove_cart=(id)=>{
       
-      const user_id=(JSON.parse(localStorage.getItem("tokken1"))[0])._id;
+      const user_id=JSON.parse(localStorage.getItem("tokken1"))._id;
       fetch("/api/cart_remove",{
         method:"POST",
         headers:{
